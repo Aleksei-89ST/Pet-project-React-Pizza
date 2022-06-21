c986ce38ad638d7b4ed5b409b279e1864fd26e62
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-06-21 23:09:17 +0400

    логика переключение по категориям

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,15 +2,12 @@ import { useState } from "react";
 
 function Categories() {
   const [activeIndex,setActiveIndex] = useState(0)
-  const onClickCategory = (index) => {
-    setActiveIndex(index)
-  }
   const categories = ['Все','Мясные','Вегетарианская','Гриль','Острые','Закрытые']
   return (
     <div className="categories">
       <ul>
       {categories.map((value,i) => 
-        <li key={i} onClick={() => onClickCategory(i)} className={activeIndex === i ? "active" : ''}>{value}</li>
+        <li key={i} onClick={() => setActiveIndex(i)} className={activeIndex === i ? "active" : ""}>{value}</li>
   )}
       </ul>
     </div>
