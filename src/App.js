8dd2ab7e635019c92611c080701896ee760183e0
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-06-23 23:37:42 +0400

    Подключил React Router v6, создал компонент-скелетон

--- src/App.js
+++ src/App.js
@@ -1,37 +1,21 @@
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
 import Header from "./components/Header";
-import { useEffect, useState } from "react";
+import Home from "./pages/Home";
+import {Routes,Route} from "react-router-dom";
 import "./scss/components/app.scss";
-import Skeleton from "./components/Pizza-Block/Skeleton";
-import PizzaBlock from "./components/PizzaBlock";
+import NotFound from "./pages/NotFound";
+import Cart from "./pages/Cart";
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    fetch("https://62b41f5aa36f3a973d2c669d.mockapi.io/items")
-      .then((res) => res.json())
-      .then((arr) => {
-        setItems(arr);
-        setIsLoading(false)
-      });
-  }, []);
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(6)].map((_,index) => <Skeleton key={index} />)
-              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-          </div>
+          <Routes>
+<Route path="/" element={<Home/>} />
+<Route path="/cart" element={<Cart/>} />
+<Route path="*" element={<NotFound/>} />
+          </Routes>
         </div>
       </div>
     </div>
@@ -39,3 +23,4 @@ function App() {
 }
 
 export default App;
+  
