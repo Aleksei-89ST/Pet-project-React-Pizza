817de34816e3bb4ecb3eb5a7c7921745350450e1
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-06-23 13:53:12 +0400

    запрос к mockAPI,  с помощью fetch

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import Header from "./components/Header";
 import PizzaBlock from "./components/PizzaBlock";
-import pizzas from "./assets/pizzas.json";
-import "./scss/app.scss";
+import { useEffect, useState } from "react";
+import "./scss/components/app.scss";
+
 
 function App() {
+  const [items, setItems] = useState([]);
+useEffect(() => {
+  fetch("https://62b41f5aa36f3a973d2c669d.mockapi.io/items")
+  .then((res) => res.json())
+  .then((arr) => {
+    setItems(arr);
+  });
+}, [])
   return (
     <div className="wrapper">
       <Header />
@@ -17,8 +26,8 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
-              <PizzaBlock key={obj.id}{...obj}/>
+            {items.map((obj) => (
+              <PizzaBlock key={obj.id} {...obj} />
             ))}
           </div>
         </div>
