1c0774f2873286fc70c430c26aaffd430ba9323e
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-06-23 23:34:33 +0400

    описание страниц

---
+++ src/pages/Home.jsx
@@ -0,0 +1,36 @@
+
+
+import Categories from "../components/Categories";
+import { useEffect, useState } from "react";
+import Skeleton from "../components/Pizza-Block/Skeleton";
+import PizzaBlock from "../components/Pizza-Block";
+import Sort from "../components/Sort";
+
+const Home = () => {
+    const [items, setItems] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+      fetch("https://62b41f5aa36f3a973d2c669d.mockapi.io/items")
+        .then((res) => res.json())
+        .then((arr) => {
+          setItems(arr);
+          setIsLoading(false)
+        });
+    }, []);
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </>
+  );
+};
+
+export default Home;
