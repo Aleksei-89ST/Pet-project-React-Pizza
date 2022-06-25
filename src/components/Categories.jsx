a5d0abf115282e5ea1d0d971d4999525739e0706
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-06-25 22:02:24 +0400

    логика сортировки по категориям

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 
-function Categories() {
+function Categories({value,onClickCategory}) {
   const [activeIndex,setActiveIndex] = useState(0)
   const categories = ['Все','Мясные','Вегетарианская','Гриль','Острые','Закрытые']
   return (
     <div className="categories">
       <ul>
-      {categories.map((value,i) => 
-        <li key={i} onClick={() => setActiveIndex(i)} className={activeIndex === i ? "active" : ""}>{value}</li>
+      {categories.map((categoryName,i) => 
+        <li key={i} onClick={() => onClickCategory(i)} className={value === i ? "active" : ""}>{categoryName}</li>
   )}
       </ul>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,19 +9,22 @@ import Sort from "../components/Sort";
 const Home = () => {
     const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [categoryId,setCategoryId] = useState(0);
+    const [sortType, setSortType] = useState(0);
     useEffect(() => {
-      fetch("https://62b41f5aa36f3a973d2c669d.mockapi.io/items")
+      setIsLoading(true)
+      fetch("https://62b41f5aa36f3a973d2c669d.mockapi.io/items?category=" + categoryId)
         .then((res) => res.json())
         .then((arr) => {
           setItems(arr);
           setIsLoading(false)
         });
         window.scrollTo(0, 0)
-    }, []);
+    }, [categoryId]);
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
+        <Categories value={categoryId} onClickCategory={(i) => setCategoryId(i)}/>
         <Sort />
       </div>
       <h2 className="content__title">Все пиццы</h2>
